8b3f6732720c525719a457bcb5c75bbbe7211d38
kasiian-37 <65597251+kasiian-37@users.noreply.github.com>
2021-03-01 23:40:24 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,131 +1,99 @@
-import { Component } from 'react';
-import Loader from 'react-loader-spinner';
-import { ToastContainer, toast } from 'react-toastify';
-import Searchbar from './components/Searchbar/Searchbar';
-import ImageGallery from './components/ImageGallery/ImageGallery';
-import fetchImages from './services/imageFinderApi';
-import Button from './components/Button/Button';
-import Modal from './components/Modal/Modal';
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import { Component } from "react";
+import { createPortal } from "react-dom";
+import { ToastContainer } from "react-toastify";
+import Searchbar from "./components/Searchbar";
+import ImageGallery from "./components/ImageGallery";
+import Modal from "./components/Modal";
+import API from "./services/imageFinderApi";
 
-const Status = {
-  IDLE: 'idle',
-  PENDING: 'pending',
-  RESOLVED: 'resolved',
-  REJECTED: 'rejected',
-};
 
+const modalRoot = document.querySelector("#modal-root");
 export default class App extends Component {
   state = {
-    searchQuery: '',
+    query: "",
     page: 1,
-    imageList: [],
-    error: '',
-    status: Status.IDLE,
+    pictures: [],
+    error: null,
+    status: "idle",
     showModal: false,
-    currentImgObj: { largeUrl: '', alt: '' },
+    modalContent: {
+      url: "",
+      alt: "",
+    },
   };
 
   componentDidUpdate(prevProps, prevState) {
-    const prevQuery = prevState.searchQuery;
-    const nextQuery = this.state.searchQuery;
+    const prevQuery = prevState.query;
+    const currentQuery = this.state.query;
 
-    if (prevQuery !== nextQuery) {
-      this.setState({
-        imageList: [],
-        status: Status.PENDING,
+    if (prevQuery !== currentQuery) {
+      this.setState({ status: "pending" });
+
+      this.fetchPictures();
+    }
+
+    if (prevState.page !== this.state.page) {
+      window.scrollTo({
+        top: document.documentElement.scrollHeight,
+        behavior: "smooth",
       });
-      this.fetchProcessing(nextQuery);
     }
   }
 
-  handleFormSubmit = searchQuery => {
-    this.setState({ searchQuery });
-    this.resetPage();
-  };
-
-  handleButtonClick = () => {
-    const nextQuery = this.state.searchQuery;
-    this.setState({ status: Status.PENDING });
-    this.fetchProcessing(nextQuery);
-  };
-
-  fetchProcessing = nextQuery => {
-    fetchImages(nextQuery, this.state.page)
-      .then(data => {
-        if (data.hits.length === 0) {
-          this.setState({ status: Status.IDLE });
-          return toast.error('Unfortunately, there is nothing to show...');
-        }
-        this.setState(prevState => ({
-          imageList: [...prevState.imageList, ...data.hits],
-          status: Status.RESOLVED,
-        }));
-      })
-      .catch(({ message }) =>
-        this.setState({ error: message, status: Status.REJECTED }),
+  fetchPictures = () => {
+    API.fetchPictures(this.state.query, this.state.page)
+      .then((res) =>
+        this.setState(({ pictures, page }) => ({
+          pictures: [...pictures, ...res.hits],
+          status: "resolved",
+          page: page + 1,
+        }))
       )
-      .finally(() => {
-        window.scrollTo({
-          top: document.documentElement.scrollHeight,
-          behavior: 'smooth',
-        });
-        this.setState(prevState => ({ page: (prevState.page += 1) }));
-      });
+      .catch((error) => this.setState({ error, status: "rejected" }));
   };
 
-  resetPage = () => {
-    this.setState({ page: 1 });
+  handleSearchSubmit = (searchQuery) => {
+    this.setState({ query: searchQuery, page: 1, pictures: [] });
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
-  };
-
-  handleGalleryItemClick = e => {
-    const largeUrl = e.target.dataset.largeUrl;
-    const alt = e.target.alt;
-    if (e.target.nodeName === 'IMG') {
-      this.setState({ currentImgObj: { largeUrl, alt } });
-    }
+  handleImageClick = (imgSrc, alt) => {
+    this.setState({ modalContent: { url: imgSrc, alt } });
     this.toggleModal();
   };
 
-  render() {
-    const { imageList, error, status, showModal, currentImgObj } = this.state;
-    return (
-      <>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        {error && <h1>{error}</h1>}
-        <ImageGallery
-          imageList={imageList}
-          onClick={this.handleGalleryItemClick}
-        />
-        {showModal && (
-          <Modal
-            src={currentImgObj.largeUrl}
-            alt={currentImgObj.alt}
-            onClose={this.toggleModal}
-          ></Modal>
-        )}
-        <div className="container">
-          {status === Status.PENDING && (
-            <Loader
-              className="loader"
-              type="ThreeDots"
-              color="#66ffff"
-              height={100}
-              width={100}
-            />
-          )}
-          {status === Status.RESOLVED && (
-            <Button onClick={this.handleButtonClick} />
-          )}
-        </div>
+  handleLoadMore = () => {
+    this.fetchPictures();
+  };
 
-        <ToastContainer autoClose={2000} />
-      </>
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
+
+  render() {
+    const { showModal, modalContent, pictures, status, error } = this.state;
+    return (
+      <div className="container">
+        <Searchbar onSubmit={this.handleSearchSubmit} />
+        <ImageGallery
+          pictures={pictures}
+          status={status}
+          error={error}
+          onClick={this.handleImageClick}
+          onLoadMore={this.handleLoadMore}
+        />
+        <ToastContainer autoClose={3000} />
+        {showModal &&
+          createPortal(
+            <Modal
+              src={modalContent.url}
+              alt={modalContent.alt}
+              onClose={this.toggleModal}
+            />,
+            modalRoot
+          )}
+      </div>
     );
   }
 }
