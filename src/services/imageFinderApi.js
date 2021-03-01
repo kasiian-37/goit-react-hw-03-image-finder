58c28c4d380a4ba68d596750d71040eefcf7a85d
kasiian-37 <65597251+kasiian-37@users.noreply.github.com>
2021-03-01 23:56:06 +0200

    Update imageFinderApi.js

--- src/services/imageFinderApi.js
+++ src/services/imageFinderApi.js
@@ -1,21 +1,18 @@
-const API_KEY = '19182122-b959857203510615d63e1aa25';
-const BASE_URL =
-  'https://pixabay.com/api/?image_type=photo&orientation=horizontal&';
-
-export default function fetchImages(searchQuery, page) {
-  const searchParams = new URLSearchParams({
-    q: searchQuery,
-    page,
-    per_page: 12,
-    key: API_KEY,
-  });
-  return fetch(`${BASE_URL}${searchParams}`).then(response => {
-    if (response.ok) {
-      return response.json();
+function fetchPictures(query, page) {
+  const BASE_URL = "https://pixabay.com/api/";
+  const API_KEY = "19182122-b959857203510615d63e1aa25";
+  return fetch(
+    `${BASE_URL}?image_type=photo&orientation=horizontal&q=${query}&page=${page}&per_page=15&key=${API_KEY}`
+  ).then((res) => {
+    if (res.ok) {
+      return res.json();
     }
-
-    return Promise.reject(
-      new Error(`There are no images for your query: ${searchQuery}`),
-    );
+    return Promise.reject(new Error("Something is wrong, please try again"));
   });
 }
+
+const API = {
+  fetchPictures,
+};
+
+export default API;
